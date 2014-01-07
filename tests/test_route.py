c165d36e5a7c50cad5ef306bf6df0c029d4d40a6
Jason Sanford <jason.sanford@mapmyfitness.com>
2014-01-06 17:07:49 -0700

    test point stuff

--- tests/test_route.py
+++ tests/test_route.py
@@ -48,6 +48,24 @@ class RouteTest(MapMyFitnessTestCase):
             self.assertIsInstance(exc, InvalidObjectException)
             self.assertEqual(str(exc), 'Route privacy must exist and be one of constants.PUBLIC, constants.PRIVATE or constants.FRIENDS.')
 
+    def test_create_bad_point(self):
+        a_route = copy.deepcopy(valid_route)
+        a_route['points'][0] = 'lobster'
+        try:
+            self.mmf.route.create(a_route)
+        except Exception as exc:
+            self.assertIsInstance(exc, InvalidObjectException)
+            self.assertEqual(str(exc), 'Each point in Route points must be of type dict.')
+
+    def test_create_bad_point_lat(self):
+        a_route = copy.deepcopy(valid_route)
+        a_route['points'][0]['lng'] = 'lobster'
+        try:
+            self.mmf.route.create(a_route)
+        except Exception as exc:
+            self.assertIsInstance(exc, InvalidObjectException)
+            self.assertEqual(str(exc), 'Each point in Route points must have a "lng" key and be of type int or float.')
+
     @httpretty.activate
     def test_create_success(self):
         content_returned = '{"total_descent": null, "city": "Littleton", "data_source": null, "description": "This is a super-simplified route of my commute.", "updated_datetime": "2014-01-05T14:59:28.670550+00:00", "created_datetime": "2014-01-05T14:59:28.598493+00:00", "country": null, "start_point_type": "", "starting_location": {"type": "Point", "coordinates": [0, 0]}, "distance": 25749.5, "total_ascent": null, "climbs": null, "state": "CO", "points": [{"lat": 39.5735, "lng": -105.0164}, {"lat": 39.6781, "lng": -104.9926}, {"lat": 39.75009, "lng": -104.99656}], "postal_code": "", "min_elevation": null, "_links": {"documentation": [{"href": "https://developer.mapmyapi.com/docs/Route"}], "privacy": [{"href": "/v7.0/privacy_option/0/", "id": "0"}], "self": [{"href": "/v7.0/route/341663347/?field_set=detailed", "id": "341663347"}], "alternate": [{"href": "/v7.0/route/341663347/?format=kml&field_set=detailed", "id": "341663347", "name": "kml"}], "user": [{"href": "/v7.0/user/9118466/", "id": "9118466"}], "thumbnail": [{"href": "//images.mapmycdn.com/routes/thumbnail/341663347?size=100x100"}]}, "max_elevation": null, "name": "My Commute"}'
