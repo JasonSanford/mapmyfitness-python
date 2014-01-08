2192949eb9202fee0a462391f3ee9976699be955
Brett Nekolny <brett.nekolny@mapmyfitness.com>
2014-01-08 07:21:39 -0700

    Removed a test that was moved.

--- tests/__init__.py
+++ tests/__init__.py
@@ -7,9 +7,3 @@ class MapMyFitnessTestCase(unittest.TestCase):
     uri_root = 'https://oauth2-api.mapmyapi.com/v7.0'
     def setUp(self):
         self.mmf = MapMyFitness(api_key='abc123', access_token='super-secret-token')
-
-    def test_MapMyFitness_is_singleton(self):
-        mmf1 = MapMyFitness('api-key1', 'access_token1')
-        mmf2 = MapMyFitness('api-key2', 'access_token2')
-        
-        assert mmf1 == mmf2
