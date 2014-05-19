f2b9d346dd778a6a720f48aa53242263a76cedb3
Jason Sanford <jasonsanford@gmail.com>
2014-05-19 10:47:08 -0400

    Add modules to packages.

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ requires = ['requests>=2.1.0']
 
 setup(
     name='mapmyfitness',
-    version='0.1.0',
+    version='0.1.1',
     description='A python wrapper for the MapMyFitness API',
     author='Jason Sanford',
     author_email='jasonsanford@gmail.com',
     url='https://github.com/JasonSanford/mapmyfitness-python',
-    packages= ['mapmyfitness',],
+    packages= ['mapmyfitness', 'mapmyfitness.api', 'mapmyfitness.objects', 'mapmyfitness.validators',],
     install_requires=requires,
     license='Apache License',
 )
